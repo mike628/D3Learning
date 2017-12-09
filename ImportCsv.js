f1ccec69abb877254a4580d89772ce6755fb661d
Mike <coder316@gmail.com>
2017-12-08 19:40:42 -0500

    Added filtering

--- ImportCsv.js
+++ ImportCsv.js
@@ -1,1 +1,11 @@
-d3.csv("./Datasets/Anne_Arundel_County_Crime_Rate_By_Type.csv",function(data){console.log(data)});
+var dataset;
+function yearFilter(min, max) {
+    return function (x) {
+        if (x.YEAR < max && x.YEAR > min) return x
+    }
+};
+d3.csv("./Datasets/Anne_Arundel_County_Crime_Rate_By_Type.csv",function(data){dataset=data;
+var years = yearFilter(1970, 1980);
+dataset = dataset.filter(years);
+console.log(dataset);
+});
