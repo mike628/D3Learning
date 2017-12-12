50b6b033651704ae9352b4061b9e4535d29a0f4b
Mike <coder316@gmail.com>
2017-12-11 19:32:41 -0500

    Added to filter function and a button to update dataset

--- ImportCsv.js
+++ ImportCsv.js
@@ -1,23 +1,47 @@
 var dataset;
+
 function yearFilter(min, max) {
     return function (x) {
-        if (x.YEAR < max && x.YEAR > min) return x
+        if (x.YEAR <= max && x.YEAR >= min) return x
     }
 };
-d3.csv("./Datasets/Anne_Arundel_County_Crime_Rate_By_Type.csv",function(data){dataset=data;
-var years = yearFilter(1970, 1980);
-dataset = dataset.filter(years);
-d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d){return d.POPULATION});
+d3.csv("./Datasets/Anne_Arundel_County_Crime_Rate_By_Type.csv", function (data) {
+    dataset = data;
+
+// d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d){return d.POPULATION});
 
 });
+
 function setMinValue(minYear, p1) {
     var x = document.getElementById(p1);
     var y = document.getElementById(minYear);
-    x.innerHTML  = y.value;
+    x.innerHTML = y.value;
 }
+
 function setMaxValue(maxYear, p2) {
     var x = document.getElementById(p2);
     var y = document.getElementById(maxYear);
-    x.innerHTML  = y.value;
+    x.innerHTML = y.value;
 }
-window.onload = function() { setMinValue('minYear', 'p1');setMaxValue('maxYear', 'p2');}
+
+window.onload = function () {
+    setMinValue('minYear', 'p1');
+    setMaxValue('maxYear', 'p2');
+}
+document.getElementById("clickMe").onclick = settimespanandrun;
+
+function settimespanandrun() {
+    var firstYear = parseInt(minYear.value);
+    var lastYear = parseInt(maxYear.value);
+    var years = yearFilter(firstYear, lastYear);
+    var yearFilterDataSet = dataset.filter(years);
+    d3.select("body").selectAll("p").data(yearFilterDataSet).enter().append("p").text(function (d) {
+        return d.YEAR + " " + d.POPULATION
+    });
+
+    function update() {
+        d3.select("body").selectAll("p").data(yearFilterDataSet).enter().append("p").text(function (d) {
+            return d.YEAR + " " + d.POPULATION
+    }
+
+}
